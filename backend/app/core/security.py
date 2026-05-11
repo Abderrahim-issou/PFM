e7059fc059b5781d88fe4176257e5aef8c5ad24a
ISSOU <abderrahimissou685@gmail.com>
2026-05-11 06:12:51 +0200

    Add security.py

---
+++ backend/app/core/security.py
@@ -0,0 +1,39 @@
+from passlib.context import CryptContext
+import os
+import jwt
+from fastapi import HTTPException;
+
+
+pwd_context = CryptContext(schemes=['bcrypt'], deprecated="auto")
+
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv('ALGORITHM')
+
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password);
+
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return pwd_context.verify(plain_password,hashed_password);
+
+def verify_token(token: str):
+    print("SECRET:", SECRET_KEY)
+    print("ALGORITHM:", ALGORITHM)
+    try:
+        
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=403,
+            detail="Token expired"
+    );
+        
+    except jwt.InvalidTokenError:
+        raise HTTPException(
+            status_code=403,
+            detail="Invalid Token"
+    );
+        
+    
