e0bcd240e7d7b24894662b944df90a80067a5315
ISSOU <abderrahimissou685@gmail.com>
2026-06-06 05:08:34 +0200

    Add PlayGroundCheck.tsx

---
+++ frontend/src/pages/PlayGroundCheck.tsx
@@ -0,0 +1,54 @@
+import { useParams } from "react-router-dom";
+import DetectedRegionsPlayground from "../components/Diagnostic_component";
+import { getDiagnosticReportById } from "../api/api";
+import useAuth from "../hooks/useAuth";
+import type { DiagnosticReport } from "../types/Global";
+
+import { useEffect, useState } from "react";
+
+const PlayGroundCheck = () => {
+  const { reportId } = useParams();
+  const { access_token } = useAuth();
+
+  const [report, setReport] = useState<any>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchReport = async () => {
+      try {
+        if (!access_token || !reportId) return;
+
+        const response = await getDiagnosticReportById(
+          Number(reportId),
+          access_token
+        );
+
+        if (response.status === 200) {
+          setReport(response.data);
+        }
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchReport();
+  }, [access_token, reportId]);
+
+  if (loading) {
+    return <div>Loading report...</div>;
+  }
+
+  if (!report) {
+    return <div>Report not found</div>;
+  }
+
+  return (
+    <DetectedRegionsPlayground
+      report={report}
+    />
+  );
+};
+
+export default PlayGroundCheck;
