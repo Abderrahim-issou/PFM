2631884231b1880f89691f643b240aa038310911
ISSOU <abderrahimissou685@gmail.com>
2026-06-25 16:52:44 +0200

    Remove unessecery loggings bfore deployment

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv('DATABASE_URL')
-print('this is the: ', DATABASE_URL);
+
 engine = create_engine(
     DATABASE_URL,
     connect_args={"check_same_thread": False}
